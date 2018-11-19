0923231083d940ce763587aa4fde68e6ebc8524b
Yan Annie Jiao <yjiao@tesla.com>
2018-11-19 13:25:31 -0800

    base

---
+++ static/js/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Hello from "./Hello";
+import { PageHeader } from "react-bootstrap";
+
+require('../css/fullstack.css');
+var $ = require('jquery');
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <PageHeader>
+                <div className='header-contents'>
+                <Hello name='Rimini' />
+                </div>
+            </PageHeader>
+        );
+    }
+}
